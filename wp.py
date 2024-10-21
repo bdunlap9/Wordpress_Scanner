2bc03c33e34ba2f608aa8c11f74ce30c8a9e8fe6
Weeke <baileydunlap9@gmail.com>
2024-10-21 00:06:01 -0700

    Update wp.py

--- wp.py
+++ wp.py
@@ -13,7 +13,7 @@ class AsyncWordPressScanner:
 
     async def fetch(self, session, url):
         try:
-            async with session.get(url, headers={"User-Agent": self.user_agent}) as response:
+            async with session.get(url, headers={'User-Agent': self.user_agent}) as response:
                 if response.status == 200:
                     return await response.text()
                 else:
@@ -24,8 +24,8 @@ class AsyncWordPressScanner:
 
     async def check_wordpress(self, session):
         response = await self.fetch(session, self.url)
-        if response and "wp-" not in response:
-            print("Not a WordPress site.")
+        if response and 'wp-' not in response:
+            print('Not a WordPress site.')
             return False
         return True
 
@@ -33,17 +33,17 @@ class AsyncWordPressScanner:
         response = await self.fetch(session, self.url)
         if response:
             self.version = await self.extract_version(response)
-            print(f"Version: {self.version}")
+            print(f'Version: {self.version}')
 
     async def check_readme(self, session):
         response = await self.fetch(session, f'{self.url}/readme.html')
         if response:
-            print(f"Readme file found at {self.url}/readme.html")
+            print(f'Readme file found at {self.url}/readme.html')
 
     async def check_debug_log(self, session):
-        response = await self.fetch(session, f"{self.url}/debug.log")
-        if response and "404" not in response:
-            print(f"Debug log file found at {self.url}/debug.log")
+        response = await self.fetch(session, f'{self.url}/debug.log')
+        if response and '404' not in response:
+            print(f'Debug log file found at {self.url}/debug.log')
 
     async def check_backup_file(self, session):
         #'*.bak', '*.backup', '*.old', '*.orig', '*.swp', 
@@ -84,57 +84,58 @@ class AsyncWordPressScanner:
         for backup_file in backup_files:
             response = await self.fetch(session, self.url + '/' + backup_file)
             if response:
-                print(f"A backup file has been found at {self.url + backup_file}")
+                print(f'A backup file has been found at {self.url + '/' + backup_file}')
 
     async def check_directory_listing(self, session):
-        directories = ["wp-content/uploads/", "wp-content/plugins/", "wp-content/themes/", "wp-includes/", "wp-admin/"]
-        dir_names = ["Uploads", "Plugins", "Themes", "Includes", "Admin"]
+        directories = ['wp-content/uploads/', 'wp-content/plugins/', 'wp-content/themes/', 'wp-includes/', 'wp-admin/']
+        dir_names = ['Uploads', 'Plugins', 'Themes', 'Includes', 'Admin']
 
         for directory, name in zip(directories, dir_names):
             response = await self.fetch(session, self.url + '/' + directory)
-            if response and "Index of" in response:
+            if response and 'Index of' in response:
                 self.files.add(directory)
-                print(f"{name} directory has directory listing enabled at {self.url + directory}")
+                print(f'{name} directory has directory listing enabled at {self.url + "/" + directory}')
 
     async def is_xml_rpc(self, session):
-        response = await self.fetch(session, f"{self.url}/xmlrpc.php")
+        response = await self.fetch(session, f'{self.url}/xmlrpc.php')
         if response:
-            print(f"XML-RPC Interface available under: {self.url}/xmlrpc.php")
+            print(f'XML-RPC Interface available under: {self.url}/xmlrpc.php')
 
     async def check_robots_text(self, session):
-        response = await self.fetch(session, f"{self.url}/robots.txt")
+        response = await self.fetch(session, f'{self.url}/robots.txt')
         if response:
-            print(f"robots.txt available under: {self.url}/robots.txt")
+            print(f'robots.txt available under: {self.url}/robots.txt')
             lines = response.split('\n')
             for l in lines:
-                if "Disallow:" in l:
-                    print(f"Interesting entry from robots.txt: {l}")
+                if 'Disallow:' in l:
+                    print(f'Interesting entry from robots.txt: {l}')
 
     async def check_full_path_disclosure(self, session):
-        response = await self.fetch(session, self.url + "/wp-includes/rss-functions.php")
+        response = await self.fetch(session, self.url + '/wp-includes/rss-functions.php')
         if response:
-            regex = re.compile("Fatal error:.*? in (.*?) on", re.S)
+            regex = re.compile('Fatal error:.*? in (.*?) on', re.S)
             matches = regex.findall(response)
 
             if matches:
                 exposed_path = matches[0].replace('\n', '').strip()
-                print(f"Full Path Disclosure (FPD) in {self.url + 'wp-includes/rss-functions.php'}")
-                print(f"Exposed Path: {exposed_path}")
+                print(f'Full Path Disclosure (FPD) in {self.url + 'wp-includes/rss-functions.php'}')
+                print(f'Exposed Path: {exposed_path}')
 
     async def enum_wordpress_users(self, session):
-        response = await self.fetch(session, f"{self.url}/wp-json/wp/v2/users")
+        response = await self.fetch(session, f'{self.url}/wp-json/wp/v2/users')
         if response:
-            print("Enumerating WordPress users")
+            print('Enumerating WordPress users')
             users = json.loads(response)
             for user in users:
-                print(f"Identified the following user: {user['id']}, {user['name']}, {user['slug']}")
+                print(f'Identified the following user: {user["id"]}, {user["name"]}, {user["slug"]}')
             self.users = users
+
 
     async def extract_version(self, response):
         match = re.search(r'Version ([0-9]+\.[0-9]+\.?[0-9]*)', response)
         if match:
             return match.group(1)
-        print("WordPress version not found in the response.")
+        print('WordPress version not found in the response.')
 
     async def crawl_sitemap_for_forms(self, session, processed_urls=None):
         if processed_urls is None:
@@ -158,7 +159,7 @@ class AsyncWordPressScanner:
 
                 if response:
                     sitemap_xml = etree.fromstring(response.encode('utf-8'))
-                    loc_elements = sitemap_xml.findall(".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc")
+                    loc_elements = sitemap_xml.findall('.//{http://www.sitemaps.org/schemas/sitemap/0.9}loc')
                     urls = [loc.text for loc in loc_elements]
 
                     forms_with_input = []
@@ -169,88 +170,88 @@ class AsyncWordPressScanner:
                             response = await self.fetch(session, sitemap_url)
 
                             if response:
-                                page_soup = BeautifulSoup(response, features="lxml")
-                                forms = page_soup.find_all("form")
+                                page_soup = BeautifulSoup(response, features='lxml')
+                                forms = page_soup.find_all('form')
 
                                 for form in forms:
-                                    if form.find("input", {"type": "text"}):
+                                    if form.find('input', {'type': 'text'}):
                                         forms_with_input.append(sitemap_url)
                                         break  
 
                     for sitemap_url in urls:
-                        if sitemap_url.endswith(".xml"):
+                        if sitemap_url.endswith('.xml'):
                             nested_forms = await self.crawl_sitemap_for_forms(session, processed_urls)
                             forms_with_input.extend(nested_forms)
 
                     return forms_with_input
                 else:
-                    print(f"Failed to fetch sitemap URL {sitemap_url}.")
+                    print(f'Failed to fetch sitemap URL {sitemap_url}.')
             else:
-                print(f"Sitemap URL {sitemap_url} has already been processed or is not found.")
+                print(f'Sitemap URL {sitemap_url} has already been processed or is not found.')
         else:
-            print(f"Failed to fetch robots.txt from {robots_url}.")
+            print(f'Failed to fetch robots.txt from {robots_url}.')
 
         return []
     
-async def check_plugins(self, session):
-    plugin_directory_url = f"{self.url}/wp-content/plugins/"
-    rest_api_plugins_url = f"{self.url}/wp-json/plugins/v1/all"
-    known_plugins = [
-        'wordpress-seo/wp-seo.php',  # Yoast SEO
-        'akismet/akismet.php',        # Akismet
-        'woocommerce/woocommerce.php', # WooCommerce
-    ]
+    async def check_plugins(self, session):
+        plugin_directory_url = f'{self.url}/wp-content/plugins/'
+        rest_api_plugins_url = f'{self.url}/wp-json/plugins/v1/all'
+        known_plugins = [
+            'wordpress-seo/wp-seo.php',  # Yoast SEO
+            'akismet/akismet.php',        # Akismet
+            'woocommerce/woocommerce.php', # WooCommerce
+        ]
 
-    response = await self.fetch(session, plugin_directory_url)
-    if response and "Index of" in response:
-        print(f"Plugin directory listing found at {plugin_directory_url}")
+        response = await self.fetch(session, plugin_directory_url)
+        if response and "Index of" in response:
+            print(f'Plugin directory listing found at {plugin_directory_url}')
 
-    response = await self.fetch(session, rest_api_plugins_url)
-    if response:
-        try:
-            plugins = json.loads(response)
-            print("Installed Plugins from REST API:")
-            for plugin in plugins:
-                print(f"{plugin['name']} - {plugin['version']}")
-        except json.JSONDecodeError:
-            print("Failed to decode JSON response from the REST API.")
-
-    for plugin in known_plugins:
-        plugin_url = f"{self.url}/wp-content/plugins/{plugin}"
-        response = await self.fetch(session, plugin_url)
+        response = await self.fetch(session, rest_api_plugins_url)
         if response:
-            print(f"Known plugin found: {plugin}")
+            try:
+                plugins = json.loads(response)
+                print('Installed Plugins from REST API:')
+                for plugin in plugins:
+                    print(f'{plugin['name']} - {plugin['version']}')
+            except json.JSONDecodeError:
+                print('Failed to decode JSON response from the REST API.')
 
-async def scan(self, checks):
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        if checks.get('wordpress'):
-            tasks.append(self.check_wordpress(session))
-        if checks.get('url'):
-            tasks.append(self.check_url(session))
-        if checks.get('readme'):
-            tasks.append(self.check_readme(session))
-        if checks.get('debug_log'):
-            tasks.append(self.check_debug_log(session))
-        if checks.get('backup_file'):
-            tasks.append(self.check_backup_file(session))
-        if checks.get('directory_listing'):
-            tasks.append(self.check_directory_listing(session))
-        if checks.get('xml_rpc'):
-            tasks.append(self.is_xml_rpc(session))
-        if checks.get('robots_text'):
-            tasks.append(self.check_robots_text(session))
-        if checks.get('full_path_disclosure'):
-            tasks.append(self.check_full_path_disclosure(session))
-        if checks.get('enum_users'):
-            tasks.append(self.enum_wordpress_users(session))
-        if checks.get('sitemap_forms'):
-            forms = await self.crawl_sitemap_for_forms(session)
-            print("Forms with input fields found at:", forms)
-        if checks.get('check_plugins'):
-            await self.check_plugins(session)
+        for plugin in known_plugins:
+            plugin_url = f'{self.url}/wp-content/plugins/{plugin}'
+            response = await self.fetch(session, plugin_url)
+            if response:
+                print(f'Known plugin found: {plugin}')
 
-        await asyncio.gather(*tasks)
+    async def scan(self, checks):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            if checks.get('wordpress'):
+                tasks.append(self.check_wordpress(session))
+            if checks.get('url'):
+                tasks.append(self.check_url(session))
+            if checks.get('readme'):
+                tasks.append(self.check_readme(session))
+            if checks.get('debug_log'):
+                tasks.append(self.check_debug_log(session))
+            if checks.get('backup_file'):
+                tasks.append(self.check_backup_file(session))
+            if checks.get('directory_listing'):
+                tasks.append(self.check_directory_listing(session))
+            if checks.get('xml_rpc'):
+                tasks.append(self.is_xml_rpc(session))
+            if checks.get('robots_text'):
+                tasks.append(self.check_robots_text(session))
+            if checks.get('full_path_disclosure'):
+                tasks.append(self.check_full_path_disclosure(session))
+            if checks.get('enum_users'):
+                tasks.append(self.enum_wordpress_users(session))
+            if checks.get('sitemap_forms'):
+                forms = await self.crawl_sitemap_for_forms(session)
+                print(f'Forms with input fields found at: {forms}')
+            if checks.get('check_plugins'):
+                await self.check_plugins(session)
+
+            await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='WordPress Scanner')
